4b2c9d9cdb5a22fc7f2d4b1cab8a87c8071c47e8
JonHS97 <jons18@ru.is>
2018-09-18 15:06:58 +0000

    moved print to same if statements as minus and subtractions. Also correctly changed the list for all valid_directions. Also added and Direction in all if statements that needed it.

--- TileTraveler.py
+++ TileTraveler.py
@@ -4,77 +4,96 @@
 #respond to that input.(if it is not a valid input re-request input)
 #If player reaches 3,1 print victory! and exit program
 
-#for i in range(1,4):
-#    for j in range(1,4):
-
 x = 1
 y = 1
 
+print("You can travel: (N)orth.")
+
 while (x,y) != (3,1):
-    if (x,y) == (1,1) or (x,y) == (2,1):
-        print("You can travel: (N) orth.")
-        valid_direction = "n" or "N"
-        Direction = str(input("Directions: "))
-        if Direction == valid_direction:
+    if (x,y) == (1,1):
+        valid_direction = ("n","N")
+        Direction = str(input("Direction: "))
+        if Direction in list(valid_direction):
             y += 1
-        else:
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif Direction != valid_direction:
+            print("Not a valid direction!")
+    if (x,y) == (2,1):
+        valid_direction = ("n","N")
+        Direction = str(input("Direction: "))
+        if Direction in list(valid_direction):
+            y += 1
+            print("You can travel: (S)outh or (W)est.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
     elif (x,y) == (1,2):
-        print("You can travel: (N) orth or (S) outh or (E) ast.")
-        Direction = str(input("Directions: "))
-        if Direction == "n" or "N":
+        valid_direction = ("n","N","s","S","e","E")
+        Direction = str(input("Direction: "))
+        if (Direction == "n" or Direction == "N") and (Direction in list(valid_direction)):
             y += 1
-        elif Direction == "s" or "S":
+            print("You can travel: (E)ast or (S)outh.")
+        elif (Direction == "s" or Direction == "S") and (Direction in list(valid_direction)):
             y -= 1
-        elif Direction == "e" or "E":
+            print("You can travel: (N)orth.")
+        elif (Direction == "e" or Direction == "E")and (Direction in list(valid_direction)):
             x += 1
-        else:
+            print("You can travel: (S)outh or (W)est.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
-    elif (x,y) == (2,2) or (x,y) == (3,3):
-        print("You can travel: (W) est or (S) outh.")
-        Direction = str(input("Directions: "))
-        if Direction == "s" or "S":
+    elif (x,y) == (2,2):
+        valid_direction = ("s","S","w","W")
+        Direction = str(input("Direction: "))
+        if (Direction == "s" or Direction == "S") and (Direction in list(valid_direction)):
             y -= 1
-        elif Direction == "w" or "W":
+            print("You can travel: (N)orth.")
+        elif (Direction == "w" or Direction == "W") and (Direction in list(valid_direction)):
             x -= 1
-        else:
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif Direction != valid_direction:
+            print("Not a valid direction!")
+    elif (x,y) == (3,3):
+        valid_direction = ("s","S","w","W")
+        Direction = str(input("Direction: "))
+        if (Direction == "s" or Direction == "S") and (Direction in list(valid_direction)):
+            y -= 1
+            print("You can travel: (N)orth or (S)outh.")
+        elif (Direction == "w" or Direction == "W") and (Direction in list(valid_direction)):
+            x -= 1
+            print("You can travel: (E)ast or (W)est.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
     elif (x,y) == (1,3):
-        print("You can travel: (E) ast or (S) outh.")
-        Direction = str(input("Directions: "))
-        if Direction == "e" or "E":
+        valid_direction = ("e","E","s","S")
+        Direction = str(input("Direction: "))
+        if (Direction == "e" or Direction == "E") and (Direction in list(valid_direction)):
             x += 1
-        elif Direction == "s" or "S":
+            print("You can travel: (E)ast or (W)est.")
+        elif (Direction == "s" or Direction == "S") and (Direction in list(valid_direction)):
             y -= 1
-        else:
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
     elif (x,y) == (2,3):
-        print("You can travel: (W) est or (E) ast.")
-        Direction = str(input("Directions: "))
-        if Direction == "e" or "E":
+        valid_direction = ("e","E","w","W")
+        Direction = str(input("Direction: "))
+        if (Direction == "e" or Direction == "E") and (Direction in list(valid_direction)):
             x += 1
-        elif Direction == "w" or "W":
+            print("You can travel: (S)outh or (W)est.")
+        elif (Direction == "w" or Direction == "W") and (Direction in list(valid_direction)):
             x -= 1
-        else:
+            print("You can travel: (E)ast or (S)outh.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
     elif (x,y) == (3,2):
-        print("You can travel: (N) orth or (S) outh.")
-        Direction = str(input("Directions: "))
-        if Direction == "s" or "S":
+        valid_direction = ("s","S","n","N")
+        Direction = str(input("Direction: "))
+        if (Direction == "s" or Direction == "S") and (Direction in list(valid_direction)):
             y -= 1
-        elif Direction == "n" or "N":
+        elif (Direction == "n" or Direction == "N") and (Direction in list(valid_direction)):
             y += 1
-        else:
+            print("You can travel: (S)outh or (W)est.")
+        elif Direction != valid_direction:
             print("Not a valid direction!")
-
-#        if (Direction == str("n") or Direction == str("N")) and valid_direction == "n":
-#            player_location = (x+0,y+1)
-#        elif (Direction == str("s") or Direction == str("S")) and valid_direction == "s":
-#            player_location = (x+0,y-1)
-#        elif (Direction == str("e") or Direction == str("E")) and valid_direction == "e":
-#            player_location = (x+1,y+0)
-#        elif (Direction == str("w") or Direction == str("W")) and valid_direction == "w":
-#            player_location = (x-1,y+0)
 else:
     print("Victory!")
     exit
