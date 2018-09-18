d5d47c2ea1acf5cfc272c2723adeba0809abe0f1
JonHS97 <jons18@ru.is>
2018-09-18 10:12:07 +0000

    Added if statements

--- TileTraveler.py
+++ TileTraveler.py
@@ -7,3 +7,29 @@
 for i in range(1,4):
     for j in range(1,4):
         print(i,j)
+
+player_start = (1,1)
+
+player_location = player_start
+
+if player_location == (1,1) or player_location == (2,1):
+    print("You can travel: (N) orth.")
+    Direction = str(input("Directions: "))
+elif player_location == (1,2):
+    print("You can travel: (N) orth or (S) outh or (W) est.")
+    Direction = str(input("Directions: "))
+elif player_location == (2,2) or player_location == (3,3):
+    print("You can travel: (W) est or (S) outh.")
+    Direction = str(input("Directions: "))
+elif player_location == (1,3):
+    print("You can travel: (E) ast or (S) outh.")
+    Direction = str(input("Directions: "))
+elif player_location == (2,3):
+    print("You can travel: (W) est or (E) ast.")
+    Direction = str(input("Directions: "))
+elif player_location == (3,2):
+    print("You can travel: (N) orth or (S) outh.")
+    Direction = str(input("Directions: "))
+else:
+    print("Victory!")
+    exit
