1be989841916a33b7357b3d313ef6c27c1a41273
JonHS97 <jons18@ru.is>
2018-09-18 10:01:44 +0000

    Added a range for tiles

---
+++ TileTraveler.py
@@ -0,0 +1,9 @@
+#First create the tile map from 1,1 to 3,3
+#Make possible moves for each tile
+#request input for direction
+#respond to that input.(if it is not a valid input re-request input)
+#If player reaches 3,1 print victory! and exit program
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,j)
